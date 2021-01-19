eaa7bff5ee8b8faefebcea88a7fbd55e4d4737c8
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-01-19 09:04:38 +0800

    Initial Commit

---
+++ app/Http/Controllers/ReadingsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Readings;
+use Illuminate\Http\Request;
+
+class ReadingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Readings  $readings
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Readings $readings)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Readings  $readings
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Readings $readings)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Readings  $readings
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Readings $readings)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Readings  $readings
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Readings $readings)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_01_18_123645_create_staff_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id();
+            $table->string('username',50)->unique();
+            $table->string('password',50);
+            $table->string('firstname',50);
+            $table->string('lastname',50);
+            $table->string('gender',50);
+            $table->string('usertype',50);
+            $table->string('email',80);
+            $table->string('address',150);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
