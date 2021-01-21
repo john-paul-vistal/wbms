8ac9977af1f1c06129f057ebff1d637bd03985ec
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-01-21 13:16:12 +0800

    Secure Routes

--- routes/api.php
+++ routes/api.php
@@ -24,12 +24,40 @@ Route::post('/login', [Authentication::class, 'login']);
 
 
 Route::group(['middleware' => ['auth:sanctum']],function(){
+
     Route::post('/logout', [Authentication::class, 'logout']);
+
+    //Unguarded Rute for Staff
+    Route::get('/staff/show/{staff}',[StaffController::class,'show']);
+    Route::put('/staff/update/{staff}',[StaffController::class,'update']);
+
+    //Customers Route
+    Route::get('/customer', [CustomerController::class, 'index']);
+    Route::get('/customer/show/{customer}', [CustomerController::class, 'show']);
+    Route::post('/customer/create', [CustomerController::class, 'store']);
+    Route::delete('/customer/delete/{customer}', [CustomerController::class, 'destroy']);
+    Route::put('/customer/update/{customer}', [CustomerController::class, 'update']);
+
+    //Transaction
+    Route::get('/transaction', [TransactionController::class, 'index']);
+    Route::get('/transaction/show/{transaction}', [TransactionController::class, 'show']);
+    Route::get('/transaction/show-transactions/{id}', [TransactionController::class, 'showTransactions']);
+    Route::post('/transaction/create/', [TransactionController::class, 'store']);
+    Route::delete('/transaction/delete/{transaction}', [TransactionController::class, 'destroy']);
+    Route::put('/transaction/update/{transaction}', [TransactionController::class, 'update']);
+    Route::put('/transaction/pay/{transaction}', [TransactionController::class, 'pay']);
+
+
 });
 
 Route::group(['middleware' => ['auth:sanctum','isadmin']],function(){
 
-    //settings
+    //Settings
+    Route::get('/settings', [SettingsController::class, 'index']);
+    Route::get('/settings/show/{settings}', [SettingsController::class, 'show']);
+    Route::post('/settings/create/', [SettingsController::class, 'store']);
+    Route::delete('/settings/delete/{settings}', [SettingsController::class, 'destroy']);
+    Route::put('/settings/update/{settings}', [SettingsController::class, 'update']);
 
     //Staff Route
     Route::get('/staff',[StaffController::class,'index']);
@@ -40,36 +68,13 @@ Route::group(['middleware' => ['auth:sanctum','isadmin']],function(){
 });
 
 
-//Settings
-Route::get('/settings', [SettingsController::class, 'index']);
-Route::get('/settings/show/{settings}', [SettingsController::class, 'show']);
-Route::post('/settings/create/', [SettingsController::class, 'store']);
-Route::delete('/settings/delete/{settings}', [SettingsController::class, 'destroy']);
-Route::put('/settings/update/{settings}', [SettingsController::class, 'update']);
-
-
-//Unguarded Rute for Staff
-Route::get('/staff/show/{staff}',[StaffController::class,'show']);
-Route::put('/staff/update/{staff}',[StaffController::class,'update']);
-
-
-//Customers Route
-Route::get('/customer', [CustomerController::class, 'index']);
-Route::get('/customer/show/{customer}', [CustomerController::class, 'show']);
-Route::post('/customer/create', [CustomerController::class, 'store']);
-Route::delete('/customer/delete/{customer}', [CustomerController::class, 'destroy']);
-Route::put('/customer/update/{customer}', [CustomerController::class, 'update']);
 
 
 
-//Transaction
-Route::get('/transaction', [TransactionController::class, 'index']);
-Route::get('/transaction/show/{transaction}', [TransactionController::class, 'show']);
-Route::get('/transaction/show-transactions/{id}', [TransactionController::class, 'showTransactions']);
-Route::post('/transaction/create/', [TransactionController::class, 'store']);
-Route::delete('/transaction/delete/{transaction}', [TransactionController::class, 'destroy']);
-Route::put('/transaction/update/{transaction}', [TransactionController::class, 'update']);
-Route::put('/transaction/pay/{transaction}', [TransactionController::class, 'pay']);
+
+
+
+
 
 
 
