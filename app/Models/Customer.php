a0af0534f15e96110c356ad8d54c724d77b94caa
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-01-21 09:00:24 +0800

    Update Customer Model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -12,7 +12,7 @@ class Customer extends Model
     protected $fillable = [
        
         'firstName',
-        'lastname',
+        'lastName',
         'address',
         'email',
         'contactNumber',
